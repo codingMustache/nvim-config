20d07ddc343704fd004ce2d25944791bc03194df
codignMustache <jorge@codingMustache.dev>
2026-03-22 16:00:21 -0700

    format

--- init.lua
+++ init.lua
@@ -324,10 +324,10 @@ require("lazy").setup({
 			icons = { mappings = vim.g.have_nerd_font },
 			-- Document existing key chains
 			spec = {
-				{ "<leader>s", group = "[S]earch", mode = { "n", "v" } },
+				{ "<leader>s", group = "[S]earch",    mode = { "n", "v" } },
 				{ "<leader>t", group = "[T]oggle" },
-				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } }, -- Enable gitsigns recommended keymaps first
-				{ "gr", group = "LSP Actions", mode = { "n" } },
+				{ "<leader>h", group = "Git [H]unk",  mode = { "n", "v" } }, -- Enable gitsigns recommended keymaps first
+				{ "gr",        group = "LSP Actions", mode = { "n" } },
 			},
 		},
 	},
@@ -370,7 +370,7 @@ require("lazy").setup({
 			{ "nvim-telescope/telescope-ui-select.nvim" },
 
 			-- Useful for getting pretty icons, but requires a Nerd Font.
-			{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+			{ "nvim-tree/nvim-web-devicons",            enabled = vim.g.have_nerd_font },
 		},
 		config = function()
 			-- Telescope is a fuzzy finder that comes with a lot of different things that
--- lua/custom/plugins/dashboard.lua
+++ lua/custom/plugins/dashboard.lua
@@ -1,34 +1,34 @@
 return {
-  'goolord/alpha-nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    local alpha = require('alpha')
-    local dashboard = require('alpha.themes.dashboard')
+	"goolord/alpha-nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-        dashboard.section.header.val = {
-[[           ⢀ ]],
-[[           ⢸ ]],
-[[           ⣿⡆  ]],
-[[          ⢰⣿⣧ ]],
-[[          ⣼⣿⣿⡄ ]],
-[[        ⣀⢀⣿⣿⣿⣇ ⣀]],
-[[     ⢠⣴⠟⠉⢸⣿⣿⣿⣿⡀⠉⠻⣦⡀]],
-[[    ⣴⠟⠁  ⣼⣿⣿⣿⣿⣇  ⠈⠻⣦]],
-[[   ⣸⡏   ⣰⣿⣿⣿⣿⣿⣿⡄   ⢹⣇]],
-[[   ⣿    ⠻⣿⣿⣿⣿⣿⡿⠁    ⣿]],
-[[   ⠋⣀⣴⣿⣦⡀⠘⢿⣿⣿⠟ ⢀⣼⣦⡀ ⠿]],
-[[  ⣠⣾⣿⣿⣿⣿⣷⣄ ⠻⠋ ⢠⣾⣿⣿⣿⣷⣤⣄⡀]],
-[[ ⢰⣿⣿⣿⡿⠿⠛⠛⠛⠂   ⠿⢿⣿⣿⣿⣿⣿⣿⡿⠿⠃]],
-[[ ⣾⠟⠉  ⠐⠶⣦⣤⣀⣀⣀⣀⣤⣴⠦]],
-[[         ⠈⠉⠉⠉⠉⠁]],
-        }
-    dashboard.section.buttons.val = {
-      dashboard.button('f', '  Find File',    ':Telescope find_files<CR>'),
-      dashboard.button('r', '  Recent Files', ':Telescope oldfiles<CR>'),
-      dashboard.button('n', '  New File',     ':ene | startinsert<CR>'),
-      dashboard.button('q', '  Quit',         ':qa<CR>'),
-    }
+		dashboard.section.header.val = {
+			[[           ⢀ ]],
+			[[           ⢸ ]],
+			[[           ⣿⡆  ]],
+			[[          ⢰⣿⣧ ]],
+			[[          ⣼⣿⣿⡄ ]],
+			[[        ⣀⢀⣿⣿⣿⣇ ⣀]],
+			[[     ⢠⣴⠟⠉⢸⣿⣿⣿⣿⡀⠉⠻⣦⡀]],
+			[[    ⣴⠟⠁  ⣼⣿⣿⣿⣿⣇  ⠈⠻⣦]],
+			[[   ⣸⡏   ⣰⣿⣿⣿⣿⣿⣿⡄   ⢹⣇]],
+			[[   ⣿    ⠻⣿⣿⣿⣿⣿⡿⠁    ⣿]],
+			[[   ⠋⣀⣴⣿⣦⡀⠘⢿⣿⣿⠟ ⢀⣼⣦⡀ ⠿]],
+			[[  ⣠⣾⣿⣿⣿⣿⣷⣄ ⠻⠋ ⢠⣾⣿⣿⣿⣷⣤⣄⡀]],
+			[[ ⢰⣿⣿⣿⡿⠿⠛⠛⠛⠂   ⠿⢿⣿⣿⣿⣿⣿⣿⡿⠿⠃]],
+			[[ ⣾⠟⠉  ⠐⠶⣦⣤⣀⣀⣀⣀⣤⣴⠦]],
+			[[         ⠈⠉⠉⠉⠉⠁]],
+		}
+		dashboard.section.buttons.val = {
+			dashboard.button("f", "  Find File", ":Telescope find_files<CR>"),
+			dashboard.button("r", "  Recent Files", ":Telescope oldfiles<CR>"),
+			dashboard.button("n", "  New File", ":ene | startinsert<CR>"),
+			dashboard.button("q", "  Quit", ":qa<CR>"),
+		}
 
-    alpha.setup(dashboard.config)
-  end,
+		alpha.setup(dashboard.config)
+	end,
 }
