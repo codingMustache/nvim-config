a080abf91c65681dc76df2df77bc143fdde5fd42
codignMustache <jorge@codingMustache.dev>
2026-03-22 14:39:46 -0700

    add to gitignore

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,31 @@
+return {
+  -- your existing alpha-nvim config stays here...
+  {
+    'goolord/alpha-nvim',
+    -- ...
+  },
+
+  -- add lualine below it
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup({
+        options = {
+          theme = 'tokyonight',
+          section_separators = { left = '', right = ''},
+          component_separators = { left = '', right ='' },
+          globalstatus = true,
+        },
+        sections = {
+          lualine_a = { 'mode', },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+      })
+    end,
+  },
+}
